ccad2e7ea45075097c24b0866f37e5aac2aba409
andreandre31 <andre.ndr31@gmail.com>
2022-04-15 10:54:40 +0700

    Add CRUD

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,17 +1,42 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
-    constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) {}
 
-    @Get()
-    getProduct() {
-        return this.productService.getAll();
-    }
+  @Get()
+  getProducts(@Query('title') title: string, @Query('price') price: number) {
+    return this.productService.getProducts(title, price);
+  }
 
-    @Post()
-    createProduct(@Body('title') title: string, @Body('description') description: string, @Body('price') price: number) {
-        return this.productService.createProduct(title, description, price);
-    }
+  @Get(':id')
+  getProductById(@Param('id') id: string) {
+      return this.productService.getProductById(id);
+  }
+
+  @Post()
+  createProduct(
+    @Body('title') title: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ) {
+    return this.productService.createProduct(title, description, price);
+  }
+
+  @Put(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body('title') title: string,
+    @Body('description') description: string,
+    @Body('price') price: number,
+  ) {
+    return this.productService.updateProduct(id, title, description, price);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+      return this.productService.deleteProduct(id);
+  }
+
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,18 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ProductService {
     private product: any[] = [];
 
-    getAll(): any[] {
-        return this.product;
+    getProducts(title: string, price: number): any[] {
+        const product = this.product.filter((product) => {
+            let isMatch = true;
+            if (title && product.title != title) {
+                isMatch = false;
+            }
+            if (price && product.price != price) {
+                isMatch = false;
+            }
+            return isMatch;
+        })
+        return product;
+    }
+
+    getProductById(id: string) {
+        const product = this.findProductById(id);
+        return this.product[product];
     }
 
     createProduct(title: string, description: string, price: number) {
         this.product.push({
+            id: uuidv4(),
             title,
             description,
             price,
         });
+    }
+
+    updateProduct(id: string, title: string, description: string, price: number) {
+        const product = this.findProductById(id);
+        this.product[product].title = title;
+        this.product[product].description = description;
+        this.product[product].price = price;
+    }
+
+    findProductById(id: string) {
+        const product = this.product.findIndex((product) =>  product.id === id);
+        if (product === -1) {
+            throw new NotFoundException(`Product with id ${id} is not found`);
+        }
+        return product;
+    }
+
+    deleteProduct(id: string) {
+        const product = this.findProductById(id);
+        this.product.splice(product, 1);
     }
 }
